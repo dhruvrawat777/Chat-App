eaedd9ece2089396d373f362a06746f9e19e8a57
Dhruv <dhruvrawat2611@gmail.com>
2020-09-28 00:46:14 +0530

    Search Screen and functionality implementation

--- lib/views/SearchScreen.dart
+++ lib/views/SearchScreen.dart
@@ -10,42 +10,6 @@ class _SearchScreenState extends State<SearchScreen> {
   TextEditingController _searchItem = TextEditingController();
   DatabaseMethods databaseMethods = DatabaseMethods();
 
-  Widget getList() {
-    return Container(
-      width: MediaQuery.of(context).size.width - 10,
-      height: MediaQuery.of(context).size.height - 200,
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Container(
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'name',
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
-                ),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(30),
-                  child: RaisedButton(
-                    color: Colors.lightBlue,
-                    onPressed: () {
-                      print('hi');
-                    },
-                    child: Text('Message'),
-                  ),
-                )
-              ],
-            ),
-          );
-        },
-        itemCount: 15,
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -93,10 +57,91 @@ class _SearchScreenState extends State<SearchScreen> {
                 ),
               ],
             ),
-            getList(),
+            Container(
+              alignment: Alignment.center,
+              child: Result(
+                username: 'dhruvrawat',
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class Result extends StatelessWidget {
+  final String username;
+  Result({this.username});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height - 230,
+      margin: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        color: Colors.lightBlue,
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: Text(
+              username,
+              textAlign: TextAlign.start,
+              style: TextStyle(fontSize: 30),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(200),
+              child: Image(
+                image: NetworkImage(
+                    'https://www.vippng.com/png/detail/356-3563531_transparent-human-icon-png.png'),
+              ),
+            ),
+          ),
+          Container(
+            width: 250,
+            height: 50,
+            margin: EdgeInsets.all(50),
+            child: FlatButton(
+              onPressed: () {},
+              child: Text('Message'),
+              color: Colors.amber,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25),
+              ),
+            ),
+          ),
+        ],
+      ),
+      /* Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            username,
+            style: TextStyle(
+              fontSize: 25,
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: RaisedButton(
+              color: Colors.lightBlue,
+              onPressed: () {
+                print('hi');
+              },
+              child: Text('Message'),
+            ),
+          )
+        ],
+      ), */
     );
   }
 }
